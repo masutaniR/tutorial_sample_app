1fb78af581375112c699e925248fe4fa41882430
masutaniR <popohu87@gmail.com>
2021-01-29 02:58:51 +0000

    [Update] cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,8 +10,14 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @currrent_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @currrent_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -19,7 +25,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,14 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_atribute(:remember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
