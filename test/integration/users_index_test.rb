c1c6e0265a1977ac2260d53d63de72e60c00b8cd
masutaniR <popohu87@gmail.com>
2021-01-30 01:50:57 +0000

    [Update] user indexページ

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "index" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    User.all.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+  end
+end
